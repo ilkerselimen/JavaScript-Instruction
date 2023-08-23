6d5ca0fb494e23288c5ae655103f89d8b0322ab0
İlker Şelimen <ilkerselimen@gmail.com>
2023-08-24 01:48:01 +0300

    day10 - functions

---
+++ day10/js/44-function-practice.js
@@ -0,0 +1,11 @@
+
+const setPrice = () => {
+    const fiyat = prompt("Please enter the price");
+    const toplamFiyat = getPrice(fiyat, 20, 10);
+    alert(`Toplam fiyat: ${toplamFiyat}`);
+};
+
+const getPrice = (urunFiyati, kdv, indirimOrani) => {
+    
+};
+
